0665598fe1e9a24e2cb5892f3260a4af95be8b49
mdelcourt <martin.delcourt@uclouvain.be>
2015-10-06 16:37:27 +0200

    Compiling script

---
+++ compile.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+if not "out" in os.listdir("."):
+  print "No 'out' folder, creating it..."
+  os.system("mkdir out")
+
+if len(sys.argv)==2:
+  print "Will only compile %s"%sys.argv[1]
+  f=sys.argv[1]
+  os.system("cp %s main.cpp"%f)
+  os.system("make")
+  os.system("mv main out/%s"%f.replace(".cpp","").replace("scripts/",""))
+  os.system("rm main.cpp main.o")
+
+else:
+  for f in os.listdir("./scripts/"):
+    if not ".cpp" in f[-4:]:
+      continue
+    print "**** Compiling %s"%f
+    os.system("cp scripts/%s main.cpp"%f)
+    os.system("make")
+    os.system("mv main out/%s"%f.replace(".cpp",""))
+    os.system("rm main.cpp main.o")
+
